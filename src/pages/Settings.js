71ecbaa11dda3bf510238ca1594ee11d2df753f0
alicehrk <alicehrk23@gmail.com>
2023-05-03 19:14:13 +0900

    계정 부분 완료

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Container = styled.div`
   width: 100%;
@@ -23,35 +24,135 @@ const WhiteBox = styled.div`
   padding: 24px 16px;
   color: #A0A0A0;
   background-color: white;
-  gap: 16px;
 `;  
 
 const SubTitle = styled.div`
   font-size: 18px;
   font-weight: 500;
-  margin-bottom: 8px;
+  margin-bottom: 24px;
   color: #333333;
 `;
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
+const StyledItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 0;
 `;
 
+const ModalWrapper = styled.div`
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 999;
+`;
+
+const ModalContent = styled.div`
+  max-width: 342px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  padding: 16px;
+  gap: 8px;
+  border-radius: 16px;
+  color: #7bab6e;
+`;
+
+const ModalButton1 = styled.button`
+  flex-grow: 1;
+  flex-basis: 0;
+  height: 66px;
+  background-color: #7bab6e;
+  border-style: none;
+  border-radius: 16px;
+  padding: 20px 0;
+  color: #ffffff;
+  font-size: 18px;
+`;
+
+const ModalButton2 = styled.button`
+  flex-grow: 1;
+  flex-basis: 0;
+  height: 66px;
+  background-color: #D0E8CB;
+  border-style: none;
+  border-radius: 16px;
+  padding: 20px 0;
+  color: #7BAB6E;
+  font-size: 18px;
+`;
+
+
 const Settings = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const openModalHandler = () => {
+    setShowModal(true);
+  };
+  
+  const handleCloseModal = () =>{
+    setShowModal(false);
+  };
+
+  const movePageHandler = () => {
+    window.location.href= '/checkInfo';
+  }
+
+  const logout = () => {
+    console.log('로그아웃 기능 구현해라~');
+  }
+
   return(
     <Container>
+      {showModal && (
+        <ModalWrapper onClick={handleCloseModal}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <img src="images/ic_logout.svg" alt="icon_logout" style={{marginTop: "8px"}}/>
+            <div
+              style={{
+                fontSize: "20px",
+                color: "#7BAB6E",
+                fontWeight: "700",
+                marginTop: "8px",
+                marginBottom: "40px",
+              }}
+            >
+              로그아웃 하시겠습니까?
+            </div>
+            <div style={{display: "flex", flexDirection: "row", width: "100%", gap: "8px"}}>
+              <ModalButton1 onClick={handleCloseModal}>
+                취소
+              </ModalButton1>
+              <ModalButton2 onClick={logout}>
+                로그아웃
+              </ModalButton2>
+            </div>
+          </ModalContent>
+        </ModalWrapper>
+        )}
       <Header>설정</Header>
       <Wrapper style={{marginTop: "10px"}}>
         <WhiteBox style={{paddingTop:"-10px"}}>
           <SubTitle>계정</SubTitle>
-          <StyledLink to="/checkInfo">
-            <div style={{display: "flex", justifyContent: "space-between"}}>
+          <StyledItem onClick={movePageHandler}>
             <div>회원정보 변경</div>
-              <img src="images/ic_next_arrow.svg" alt="next_arrow_icon" />
-            </div>
-          </StyledLink>
-          <div>로그아웃</div>
+            <img src="images/ic_next_arrow.svg" alt="next_arrow_icon" />
+          </StyledItem>
+          <StyledItem onClick={openModalHandler}>
+            <div>로그아웃</div>
+            <img src="images/ic_next_arrow.svg" alt="next_arrow_icon" />
+          </StyledItem>
         </WhiteBox>
         <WhiteBox>
           <SubTitle>알림</SubTitle>
