fe338b6979ee449886f4b994e96ddaf6302e79b0
alicehrk <alicehrk23@gmail.com>
2023-05-06 23:59:11 +0900

    이용약관 temp

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
             <Route path="/history" element={<History />}></Route>
             <Route path="/locker" element={<Locker />}></Route>
             <Route path="/checkInfo" element={<CheckInfo />}></Route>
+            <Route path="/tos" element={<ToS />}></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </BrowserRouter>
