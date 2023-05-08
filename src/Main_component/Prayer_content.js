740a49055fdb261459eba32e8ac162bc6327314a
jungmook <wjdanr0869@naver.com>
2023-05-08 10:26:03 +0900

    Main_component 수정

---
+++ src/Main_component/Prayer_content.js
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react";
+import { MdAddCircle } from "react-icons/md";
+import {GiPlantSeed} from 'react-icons/gi'; 
+import styled from "styled-components";
+import { style } from "@mui/system";
+import Rectangle_img from "../images/Rectangle_img.svg"
+import Logo from "./Logo";
+import Share_mode from "./Share_mode";
+
+const Main_Content = styled.div`
+    display: flex;
+    width: 350px;
+    height: 30px;
+    margin-left : 16px;
+    margin-right : 16px;
+    margin-top : 22px; 
+`
+
+const Name_content = styled.div`
+    width: 34px;
+    height: 17px;
+    margin-right : 17px;
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 17px;
+`;
+
+const Text_content = styled.div`
+    width: 246px;
+    padding : 0px;
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+`;
+
+const Dday_content = styled.div`
+    width : 30px;
+    font-size : 10px;
+    heigth : 20px;
+    margin-right : 20px;
+`;
+const Click_img = styled(Logo)``;
+
+function Prayer_content({content, day_toggle , count_update, bottom}){
+    const {id, dday,text,checked, name, count} = content;
+    const image_ref = useRef(null);
+    return(
+        <Main_Content>
+            <Name_content>{name}</Name_content>
+            <Text_content>{text}</Text_content>
+            {day_toggle ? <Dday_content>{"D-"+ dday}</Dday_content> : <Dday_content>{count + "회"}</Dday_content>}
+            {!bottom && <div className="image"><Click_img src={Rectangle_img} onClick={() => count_update(id)}/></div>}
+        </Main_Content>
+    )
+}
+
+export default Prayer_content;
--- src/Main_component/ShareMode.js
+++ src/Main_component/ShareMode.js
@@ -31,10 +31,17 @@ const ShareMode = ({onMove, onShare, onCheck ,isShare}) =>{
     } 
 
     return(
+<<<<<<< HEAD:src/Main_component/ShareMode.js
         cancel_toggle && <div style={{display: "flex", width:"430px", height:"153px", border:"none", position: "absolute" 
         , bottom: '-168px', backgroundColor:"white", borderRadius:"24px 24px 0px 0px"}}>
             <BoxContainer onClick={() => onCancle()}>취소하기<ShareClickLogo style={{marginLeft: '20px'}} src={share_cancel}/></BoxContainer>
             <BoxContainer onClick={() => onShare()}>공유하기<ShareClickLogo style={{marginLeft: '20px'}}src={share_move}/></BoxContainer>
+=======
+        cancel_toggle && <div style={{display: "flex", width:"430px", height:"90px", border:"none", position: "absolute" 
+        ,bottom:'-30px', backgroundColor:"white", borderRadius:"24px 24px 0px 0px"}}>
+            <Box_container onClick={() => onCancle()}>취소하기<Share_click_logo style={{marginLeft: '20px'}} src={share_move}/></Box_container>
+            <Box_container onClick={() => onShare()}>공유하기<Share_click_logo style={{marginLeft: '20px'}}src={share_cancel}/></Box_container>
+>>>>>>> 1aea316 (Main_component 수정):src/Main_component/Share_mode.js
         </div>
     )
 }
