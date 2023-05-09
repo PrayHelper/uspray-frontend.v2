6acb31eda92ff0acd273942ffe0e3c9cd1cf9cf0
jungmook <wjdanr0869@naver.com>
2023-05-09 22:50:39 +0900

    폴더 이동 및 삭제

--- prayhelper-frontend/src/Main_component/serverapi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseURL = process.env.REACT_APP_API_ORIGIN + process.env.REACT_APP_API_DEFAULT_PREFIX
-const serverapi = axios.create();
-
-serverapi.defaults.baseURL = baseURL
-
-export default serverapi;
