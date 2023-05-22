1a6575b4038ff0226dfdf5b3d6ac2e8b22a091c9
alicehrk <alicehrk23@gmail.com>
2023-05-23 02:09:58 +0900

    api.js 추가

---
+++ src/hooks/api.js
@@ -0,0 +1,67 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import serverapi from "../api/serverapi";
+import { tokenState } from "../recoil/accessToken";
+
+// get 방식의 axios 호출 및 error handling
+const useGetFetcher = async (url, onSuccess) => {
+  const accessToken = useRecoilValue(tokenState);
+  const setAccessToken = useSetRecoilState(tokenState);
+  try {
+    const res = await serverapi.get(url, {
+      headers: {
+        Authorization: `${accessToken}`,
+      }
+    });
+    if (onSuccess)
+      onSuccess();
+  } catch (e) {
+    // 403 : access toekn 만료
+    if (e.status === 403) {
+      const data = refresh();
+      setAccessToken(data);
+    }
+  }
+};
+
+// post 방식의 axios 호출 및 error handling
+const usePostFetcher = async (url, data, onSuccess) => {
+  const accessToken = useRecoilValue(tokenState);
+  const setAccessToken = useSetRecoilState(tokenState);
+  try {
+    const res = await serverapi.post(url, data, {
+      headers: {
+        Authorization: `${accessToken}`,
+      }
+    });
+    if (onSuccess)
+      onSuccess();
+  } catch (e) {
+    // 403 : access toekn 만료
+    if (e.status === 403) {
+      const data = refresh();
+      setAccessToken(data);
+    }
+  }
+};
+
+// accessToken 재발급을 위한 axios 호출
+const refresh = async (status) => {
+  
+  try {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const res = await serverapi.get('/user/token', {
+      headers: {
+        Authorization: `${refreshToken}`,
+      }
+    });
+    return (res.data.access_token);
+
+  } catch (e) {
+    // 401 : refresh token 만료
+    if (e.status === 401) {
+      localStorage.setItem('refreshToken', '');
+      window.location.href('/');
+    }
+    return (e);
+  }
+};
