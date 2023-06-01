a25efb33222811d029986b5b65bd1e60b939b128
jaymoonn <sjc01214@naver.com>
2023-06-02 01:24:22 +0900

    보관함 2차 디자인 수정

---
+++ src/components/Locker/LockerContent.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const LockerContent = (props) => {
+  return (
+    <LockerContentWrapper isClicked={props.isClicked}>
+      <TopContentWrapper>
+        <Name>{props.target}</Name>
+        <NameNDate>{props.name}, D+3</NameNDate>
+      </TopContentWrapper>
+      <Content>{props.title}</Content>
+    </LockerContentWrapper>
+  );
+};
+
+export default LockerContent;
+
+const LockerContentWrapper = styled.div`
+  cursor: pointer;
+  padding: 16px;
+  background: ${(props) =>
+    props.isClicked ? "rgba(255, 255, 255, 0.75)" : "#ffffff"};
+  box-shadow: 0px 2px 8px rgba(49, 65, 44, 0.25);
+  border-radius: 16px;
+  margin: 12px 24px;
+  outline: ${(props) => (props.isClicked ? "2px solid #7BAB6E" : "")};
+`;
+
+const TopContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 5px;
+`;
+
+const Name = styled.div`
+  color: #7bab6f;
+  font-size: 12px;
+  /* line-height: 17px; */
+`;
+
+const NameNDate = styled.div`
+  color: #a0a0a0;
+  font-size: 12px;
+`;
+
+const Content = styled.div`
+  color: #606060;
+  font-size: 12px;
+  line-height: 17px;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
--- src/pages/Locker.js
+++ src/pages/Locker.js
@@ -1,47 +1,46 @@
 import React, { useEffect, useState } from "react";
-import styled, { css } from "styled-components";
-import LockerHeader from "../components/Locker/L_Header";
-import PrayChecker from "../components/Locker/L_PrayChecker";
+import styled from "styled-components";
 import serverapi from "../api/serverapi";
 import Header from "../components/Header/Header";
+import LockerContent from "../components/Locker/LockerContent";
 
 const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjBkNzI5NzQ4LTk3OTQtNGRjMC05NDk3LTY5MWEwMzk3N2Y5ZiIsImFjY2Vzc190b2tlbl9leHAiOiIyMDIzLTA1LTE4VDE1OjE5OjQ2LjM4Njc1NSJ9.o6fGb8W8dcxc9PTP5NTjB4JowayBPASaBDMP_dYkXvI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImMxNDFkYWNkLTg1NWItNDIyYy04NmIxLWFiZWRlMTQwNTEwOCIsImFjY2Vzc190b2tlbl9leHAiOiIyMDIzLTA2LTAyVDE2OjA0OjAyLjAwOTI3OCJ9.CwHk-xJaxr8FWXED1Lq7bd6JjwuO2jed4QlETHrScnk";
 
 const Locker = () => {
-  const [data, setData] = useState([1]);
+  const [data, setData] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   const isEmptyData = (data) => {
     return data.length === 0 ? true : false;
   };
-  // const getSharedPrayList = async () => {
-  //   const api = "/share";
-  //   try {
-  //     const res= await serverapi.get(api, { headers: {
-  //       'Authorization': `${accessToken}`}});
-  //     if (res.status === 200) {
-  //       var data_ = [];
-  //       data_ = res.data;
-  //       setData(data_);
-  //       console.log(data);
-  //     }
-  //   } catch (e){
-  //     console.log(e);
-  //   }
-  // };
-  // useEffect(() => {
-  //   getSharedPrayList();
-  // }, []);
-  /* <div>
-          {data.map((item) => (
-            <PrayChecker
-              name={item.share_name}
-              title={item.title}
-              dday={item.shared_at}
-              key={item.pray_id}
-            />
-          ))}
-        </div> */
+
+  const onClickContent = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const getSharedPrayList = async () => {
+    const api = "/share";
+    try {
+      const res = await serverapi.get(api, {
+        headers: {
+          Authorization: `${accessToken}`,
+        },
+      });
+      if (res.status === 200) {
+        var data_ = [];
+        data_ = res.data;
+        setData(data_);
+        console.log(data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getSharedPrayList();
+  }, []);
+
   return (
     <LockerWrapper>
       <Header>보관함</Header>
@@ -53,24 +52,18 @@ const Locker = () => {
       )}
       {!isEmptyData(data) && (
         <LockerList>
-          <LockerContent>
-            <TopContentWrapper>
-              <Name>이종우</Name>
-              <NameNDate>배서현, D+3</NameNDate>
-            </TopContentWrapper>
-            <Content>
-              안녕할 수 있도록 두 세 줄짜리는 어떻게 되느냐가 궁금하다
-              하셔가지고 이렇게 두 줄짜리를 만들어 보았씁니다. 근데 세 줄짜리가
-              더 좋아서 세줄로 함.
-            </Content>
-          </LockerContent>
-          <LockerContent>
-            <TopContentWrapper>
-              <Name>이종우</Name>
-              <NameNDate>배서현, D+3</NameNDate>
-            </TopContentWrapper>
-            <Content>한줄짜리입니다</Content>
-          </LockerContent>
+          {data.map((item) => (
+            <div onClick={onClickContent}>
+              <LockerContent
+                isClicked={isClicked}
+                name={item.share_name}
+                title={item.title}
+                target={item.target}
+                dday={item.shared_at}
+                key={item.pray_id}
+              />
+            </div>
+          ))}
         </LockerList>
       )}
     </LockerWrapper>
@@ -116,42 +109,5 @@ const LockerList = styled.div`
   align-items: center;
   height: 100%;
   width: 100%;
-`;
-
-const LockerContent = styled.div`
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0px 2px 8px rgba(49, 65, 44, 0.25);
-  border-radius: 16px;
-  margin: 6px 24px;
-  width: calc(100% - 64px);
-`;
-
-const TopContentWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100%;
-  margin-bottom: 5px;
-`;
-
-const Name = styled.div`
-  color: #7bab6f;
-  font-size: 12px;
-  /* line-height: 17px; */
-`;
-
-const NameNDate = styled.div`
-  color: #a0a0a0;
-  font-size: 12px;
-`;
-
-const Content = styled.div`
-  color: #606060;
-  font-size: 12px;
-  line-height: 17px;
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
+  overflow: auto;
 `;
