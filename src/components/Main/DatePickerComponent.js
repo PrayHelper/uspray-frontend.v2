f38f29cc1dbbc194b0c2c8326813ea6dc01e7771
jungmook <wjdanr0869@naver.com>
2023-06-01 21:29:31 +0900

    날짜 클릭했을 때 바로 띄우게 수정

--- src/components/Main/DatePickerComponent.js
+++ src/components/Main/DatePickerComponent.js
@@ -19,15 +19,16 @@ const DatePickerComponent = ({startDate, dateClick}) => {
     return(
       <DateStyle>
 			 <MydatePicker
-        showPopperArrow = {false}
         locale={ko}    // 언어설정 기본값은 영어
         dateFormat="yyyy-MM-dd"    // 날짜 형식 설정
         className="input-datepicker"    // 클래스 명 지정 css주기 위해
         minDate={new Date()}    // 선택할 수 있는 최소 날짜값 지정 
         closeOnScroll={true}    // 스크롤을 움직였을 때 자동으로 닫히도록 설정 기본값 false
-        placeholderText="체크인 날짜 선택"    // placeholder
+        // placeholderText="체크인 날짜 선택"    // placeholder
         selected={startDate}    // value
+        popperPlacement="bottom-start"
         onChange={(date) => dateClick(date)}    // 날짜를 선택하였을 때 실행될 함수
+        inline
       />
 		</DateStyle>
     );
