1511406a1774d38874cb13751dd28fd9f2ae3ba3
dongpal <dongpari21@gmail.com>
2023-07-16 11:25:15 +0900

    id bold체로 만들기

--- src/components/Find/FindIdResult.js
+++ src/components/Find/FindIdResult.js
@@ -10,83 +10,82 @@ import serverapi from "../../api/serverapi";
 import { set } from "date-fns";
 
 const Box = styled.div`
-    display: flex;
-    justify-content: center;
-    height: auto;
-    margin: 0 16px; 
-    margin-top : 22px;
-    margin-bottom: 12px;
-    border-bottom : 1px solid #D0E8CB;
-    contents-align: center;
-    `
-  const BoxSetting = styled.div`
-    color: #7BAB6E;
-    text-align: center;
-    margin-bottom: 16px;
+  display: flex;
+  justify-content: center;
+  height: auto;
+  margin: 0 16px;
+  margin-top: 22px;
+  margin-bottom: 12px;
+  border-bottom: 1px solid #d0e8cb;
+  contents-align: center;
+`;
+const BoxSetting = styled.div`
+  color: #7bab6e;
+  text-align: center;
+  margin-bottom: 16px;
 
-    font-family: Noto Sans KR;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-  `
-  
+  font-family: Noto Sans KR;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
 const FindIdResult = () => {
   // const location = useLocation();
   // const userInfo = location.state.userInfo;
   const navigate = useNavigate();
   const result = useLocation();
-  const [text, setText]= useState("");
+  const [id, setText] = useState("");
+  const [name, setText1] = useState("");
 
-  // const isAllValid = 
+  // const isAllValid =
   //   !invalidUserInfo
-
   const showId = async () => {
     const api = "/user/find/id";
     const data = {
       name: result.state.userInfo.name,
-      phone: result.state.userInfo.phoneNumber.replace(/-/g, "")
+      phone: result.state.userInfo.phoneNumber.replace(/-/g, ""),
     };
     try {
       const res = await serverapi.post(api, data);
       if (res.status === 200) {
-        setText(data.name+"님의 아이디는 "+res.data.message+"입니다.");
+        setText(res.data.message);
+        setText1(data.name);
         console.log(res.data);
       }
     } catch (err) {
-        setText("해당 정보와 일치하는 유저가 없습니다.");
+      setText("해당 정보와 일치하는 유저가 없습니다.");
       console.log(data);
       console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
     showId();
   }, []);
-  
 
   return (
     <div>
-      <UserHeader children={"아이디 찾기"}/>
-        <Box>
-          <BoxSetting>{text}</BoxSetting>
-        </Box>
-        
-        <Link to="/findPW" style={{ textDecoration: "none" }}>
-          <FindButton
-            children={"비밀번호 찾기"}
-            buttonSize={ButtonSize.LARGE}
-            buttonTheme={ButtonTheme.RED}
-          />
-        </Link>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <Button
-            buttonSize={ButtonSize.LARGE}
-            buttonTheme={ButtonTheme.GREEN}
-          >
-            메인화면으로 이동          
-          </Button>
-        </Link>
+      <UserHeader children={"아이디 찾기"} />
+      <Box>
+        <BoxSetting>{name}님의 아이디는 <BoldText children={id}/> 입니다.</BoxSetting>
+      </Box>
+      <Link to="/findPW" style={{ textDecoration: "none" }}>
+        <FindButton
+          children={"비밀번호 찾기"}
+          buttonSize={ButtonSize.LARGE}
+          buttonTheme={ButtonTheme.RED}
+        />
+      </Link>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button buttonSize={ButtonSize.LARGE} buttonTheme={ButtonTheme.GREEN}>
+          메인화면으로 이동
+        </Button>
+      </Link>
     </div>
   );
 };
