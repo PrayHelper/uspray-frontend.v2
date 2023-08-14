2855b6f3d7b18df5d8bbf65a0c1eca290615278e
jungmook <wjdanr0869@naver.com>
2023-08-15 00:20:02 +0900

    이름 디폴트 해제 api 추가

---
+++ src/hooks/useGetInfo.js
@@ -0,0 +1,31 @@
+import { useQuery } from 'react-query';
+import { getFetcher } from "./api";
+import useAuthToken from "./useAuthToken";
+import useRefresh from "./useRefresh";
+
+const getInfo = async (getAccessToken) => {
+  return await getFetcher(`/user/info`, {
+    Authorization: getAccessToken(),
+  });
+
+}
+
+export const useGetInfo = () => {
+  const { getAccessToken } = useAuthToken();
+  const { refresh } = useRefresh();
+  return useQuery([], () => {return getInfo(getAccessToken)} ,  {
+    onError: async (e) => {
+      if (e.status === 403) {
+       await refresh();
+      }
+    },
+    onSuccess: (res) => {
+      console.log(res);
+    },
+    retry: (cnt) => {
+      return cnt < 3;
+    },
+    retryDelay: 300,
+    refetchOnWindowFocus: false,
+  })
+}
