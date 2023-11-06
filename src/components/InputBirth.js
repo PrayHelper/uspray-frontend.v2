3ec461afdb56b65f6a55c93e55b9a88ba6012fc9
alicehrk <alicehrk23@gmail.com>
2023-11-06 15:43:33 +0900

    refact: change InputBirth css

--- src/components/InputBirth.js
+++ src/components/InputBirth.js
@@ -6,21 +6,21 @@ const InputContainerStyle = styled.div`
   height: 100%;
   border: 1px solid #7BAB6E;
   border-radius: 16px;
-  padding: 16.5px 14px;
+  padding: 16px 14px;
 `
 
 const InputBirthStyle = styled.input`
   border: none;
   border-bottom: 1px solid #7BAB6E;
+  border-radius: 0;
   &:focus {
     outline: none;
   };
   width: fit-content;
-  min-width: 20px;
-  max-width: 20px;
+  width: 20px;
   color: #7BAB6E;
   font-size: 16px;
-  margin-right: 5px;
+  padding: 0px 2px;
 `
 
 const InputBirth = ({yearValue, monthValue, dayValue, yearChangeHandler, monthChangeHandler, dayChangeHandler}) => {
@@ -29,17 +29,17 @@ const InputBirth = ({yearValue, monthValue, dayValue, yearChangeHandler, monthCh
       <InputContainerStyle>
         <span style={{zIndex: "1", backgroundColor: "white", fontSize: "12px", color: "#7BAB6E", position: "absolute", top: "0", transform: "translate(-5px, -5px)", padding: "0 5px" }}>생년월일</span>
         <div style={{display: "flex", width: "100%", justifyContent:"space-evenly", alignItems: "center"}}>
-          <div style={{color: "#5C6E56"}}>
-            <InputBirthStyle value={yearValue} onChange={yearChangeHandler} style={{maxWidth: "38px"}} type="number"/>
+          <div style={{display: "flex", gap: "5px", color: "#5C6E56"}}>
+            <InputBirthStyle value={yearValue} onChange={yearChangeHandler} style={{width: "38px"}} type="number"/>
             년
           </div>
           <div style={{width: "16px", transform: "rotate(90deg)", borderBottom: "1px solid #7BAB6E"}}></div>
-          <div style={{color: "#5C6E56"}}>
+          <div style={{display: "flex", gap: "5px", color: "#5C6E56"}}>
             <InputBirthStyle value={monthValue} onChange={monthChangeHandler} type="number"/>
             월
           </div>
           <div style={{width: "16px", transform: "rotate(90deg)", borderBottom: "1px solid #7BAB6E"}}></div>
-          <div style={{color: "#5C6E56"}}>
+          <div style={{display: "flex", gap: "5px", color: "#5C6E56"}}>
             <InputBirthStyle value={dayValue} onChange={dayChangeHandler} type="number"/>
             일
           </div>
