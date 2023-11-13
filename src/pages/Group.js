46851a00f181311124ede1cf4521e717daf90d3c
subin <200516bb@gmail.com>
2023-11-13 19:46:32 +0900

    feat: add GroupItem component

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -1,35 +1,53 @@
 import Header from '../components/Header/Header';
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import GroupItem from '../components/Group/GroupItem';
 
 const Group = () => {
+  const isData = true;
   return (
     <GroupWrapper>
       <Header>모임</Header>
-      <NoGroupWrapper>
-        <div style={{color: "var(--color-dark-green)", fontSize: "28px", fontWeight: "700"}}>참여하신 모임이 없어요.</div>
-        <div style={{color: "var(--color-secondary-green)", fontSize: "20px"}}>모임에 참여해서 기도제목을 공유해보세요!</div>
-      </NoGroupWrapper>
+      {
+        isData ?
+          <GroupItemWrapper>
+            <GroupItem />
+            <GroupItem />
+          </GroupItemWrapper>
+          :
+          <NoGroupWrapper>
+            <div style={{color: "var(--color-dark-green)", fontSize: "28px", fontWeight: "700"}}>참여하신 모임이 없어요.</div>
+            <div style={{color: "var(--color-secondary-green)", fontSize: "20px"}}>모임에 참여해서 기도제목을 공유해보세요!</div>
+          </NoGroupWrapper>
+      }
     </GroupWrapper>
   );
 };
-
-export default Group;
-
 const GroupWrapper = styled.div`
   /* padding-top: 65px; */
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
   width: 100%;
+  height: 100vh;
   background-color: var(--color-light-green);
-  color
 `;
 
 const NoGroupWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100vh;
   justify-content: center;
   align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
+
+const GroupItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  padding-top: 80px;
+  gap: 16px;
+`;
+
+export default Group;
+
