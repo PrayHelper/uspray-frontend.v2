3cb11829a8d500f6d1722bcdceca3eea7ab4f087
subin <200516bb@gmail.com>
2023-11-20 22:14:38 +0900

    feat: 모임 설정 navigate 추가

--- src/pages/GroupSettings.js
+++ src/pages/GroupSettings.js
@@ -3,23 +3,43 @@ import styled from 'styled-components';
 import Button from '../components/Button';
 import { ButtonSize, ButtonTheme } from '../components/Button/Button';
 import UserHeader from '../components/UserHeader';
+import { useNavigate } from 'react-router-dom';
 
 const GroupSettings = () => {
+  const navigate = useNavigate();
   return (
     <Wrapper>
       <UserHeader>모임 설정하기</UserHeader>
       <ButtonWrapper>
         <div style={{padding: "0 16px", display: "flex", flexDirection: "column", gap: "24px",}}>
-          <Button buttonSize={ButtonSize.LARGE} buttonTheme={ButtonTheme.GREEN} isArrow={true}>
+          <Button
+            buttonSize={ButtonSize.LARGE}
+            buttonTheme={ButtonTheme.GREEN}
+            isArrow={true}
+            handler={() => navigate('/changeGroupName')}
+          >
             모임 이름 변경하기
           </Button>
-          <Button buttonSize={ButtonSize.LARGE} buttonTheme={ButtonTheme.GREEN} isArrow={true}>
+          <Button
+            buttonSize={ButtonSize.LARGE}
+            buttonTheme={ButtonTheme.GREEN}
+            isArrow={true}
+            handler={() => navigate('/assignGroupLeader')}
+          >
             모임 리더 맡기기
           </Button>
-          <Button buttonSize={ButtonSize.LARGE} buttonTheme={ButtonTheme.WHITE} isArrow={true}>
+          <Button
+            buttonSize={ButtonSize.LARGE}
+            buttonTheme={ButtonTheme.WHITE}
+            isArrow={true}
+          >
             멤버 내보내기
           </Button>
-          <Button buttonSize={ButtonSize.LARGE} buttonTheme={ButtonTheme.WHITE} isArrow={true}>
+          <Button
+            buttonSize={ButtonSize.LARGE}
+            buttonTheme={ButtonTheme.WHITE}
+            isArrow={true}
+          >
             모임 삭제하기
           </Button>
         </div>
