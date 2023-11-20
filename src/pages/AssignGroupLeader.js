97b500259fbe77b08604f9103a0ed153544081da
subin <200516bb@gmail.com>
2023-11-20 22:10:27 +0900

    feat: AssignGroupLeader 페이지에 Modal, Toast 추가

--- src/pages/AssignGroupLeader.js
+++ src/pages/AssignGroupLeader.js
@@ -5,14 +5,46 @@ import Button, {ButtonSize, ButtonTheme} from '../components/Button/Button';
 import { useState } from 'react';
 import Search from '../components/AssignGroupLeader/Search';
 import SearchList from '../components/AssignGroupLeader/SearchList';
+import BlackScreen from "../components/BlackScreen/BlackScreen";
+import Modal from '../components/Modal/Modal';
+import useToast from '../hooks/useToast';
+import { ToastTheme } from '../components/Toast/Toast';
 
 const AssignGroupLeader = () => {
+  const [showModal, setShowModal] = useState(false);
   const [leader, setLeader] = useState("");
   const [searchName, setSearchName] = useState("");
+  const { showToast } = useToast({});
   const data = ['김은혜', '권은혜', '박은혜', '이은혜', '허은혜', '허그레이스', '권은혜', '박은혜', '이은혜'];
 
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <Wrapper>
+      {showModal && (
+        <>
+          <BlackScreen isModalOn={showModal} onClick={closeModal} />
+          <Modal
+            isModalOn={showModal}
+            iconSrc={"images/ic_group_leader_modal.svg"}
+            iconAlt={"icon_group_leader"}
+            mainContent={"모임 리더를 맡기시겠습니까?"}
+            subContent={"맡기신 후에는 기본 멤버로 변경됩니다."}
+            btnContent={"변경하기"}
+            btnContent2={"취소"}
+            onClickBtn={() => {
+              closeModal();
+              showToast({
+                message: "리더가 변경되었어요.",
+                theme: ToastTheme.SUCCESS,
+              });
+            }}
+            onClickBtn2={closeModal}
+            modalTheme={0}
+          />
+        </>
+      )}
       <UserHeader>모임 리더 맡기기</UserHeader>
       <ContentWrapper>
         <div style={{display: "flex", flexDirection: "column", height: '100%'}}>
@@ -27,8 +59,9 @@ const AssignGroupLeader = () => {
               buttonSize={ButtonSize.LARGE}
               buttonTheme={leader ? ButtonTheme.GREEN : ButtonTheme.GRAY}
               isArrow={true}
-              onClick={() => {
-
+              handler={() => {
+                console.log('haha');
+                setShowModal(true);
               }}
             >
               모임 리더 맡기기
